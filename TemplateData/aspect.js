95e6edfb5b93802a77176358e184850b31e70afd
Aleksandr Polianskii <drsage.pro@gmail.com>
2026-04-22 14:19:51 +0200

    214

--- TemplateData/aspect.js
+++ TemplateData/aspect.js
@@ -5,11 +5,11 @@
     const container = document.getElementById("unity-container");
     if (!container) return;
 
-    let w = window.innerWidth;
-    let h = window.innerHeight;
+    let w = window.innerWidth * 0.85;
+    let h = window.innerWidth * MIN_ASPECT; //window.innerHeight;
 
-    if (w / h < MIN_ASPECT) {
-      h = w / MIN_ASPECT;
+    //if (w / h < MIN_ASPECT) {
+    //  h = w / MIN_ASPECT;
     }
 
     container.style.width = w + "px";
